718343045091ee6cf34dc29e46f1702185093b48
looeyWei <congwei_huang@16.com>
2019-12-17 13:38:14 +0800

    Update combinatorialNumber.cpp

--- Math/combinatorialNumber.cpp
+++ Math/combinatorialNumber.cpp
@@ -1,2 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
+int n, m;
+
+ll C(int n, int m) {
+    /*
+     * 计算组合数C(n,m)
+     */
     ll res = 1;
+    for (int i = 0; i < m; i++) {
+        res = res * (n - i) / (i + 1);
+    }
+    return res;
+}
+int main() {
+    cin >> n >> m;
+    cout << C(n, m) << endl;
+    return 0;
+}
